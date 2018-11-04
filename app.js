4a9e16d7299050a1e5920ad0c246ef3ce31a1bed
rabierre <rabierre@gmail.com>
2018-11-04 19:32:59 +0900

    Setting cors

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
+
 
 require('dotenv').load();
 
@@ -10,7 +12,6 @@ var indexRouter    = require('./routes/index');
 var cardsRouter    = require('./routes/cards');
 var usersRouter    = require('./routes/users');
 var projectsRouter = require('./routes/projects');
-
 
 var app = express();
 
@@ -23,7 +24,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+// app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors({
+  'allowedHeaders': ['sessionId', 'Content-Type'],
+  'exposedHeaders': ['Content-Range', 'X-Content-Range'],
+  'origin': ['http://localhost', 'http://localhost:3000', 'http://playground.ds', 'http://playground.ds:3000'],
+  'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  'credentials': true,
+  'preflightContinue': false
+}));
 
 app.use('/', indexRouter);
 app.use('/cards', cardsRouter);
