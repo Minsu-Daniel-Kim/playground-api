1dd4d942727903e15767661c1340a19ce444cf41
rabierre <rabierre@gmail.com>
2018-11-04 22:11:34 +0900

    Fix error

--- models/cards.js
+++ models/cards.js
@@ -70,7 +70,7 @@ cardSchema.methods.shorten = function () {
     title: this.title,
     description: truncate.apply(this.description, [100, true]),
     state: this.state,
-    label: this.point.toString(),
+    label: (this.point || 0).toString(),
     assigneeId: this.assigneeId
   }
 };
@@ -96,7 +96,7 @@ cardSchema.methods.detail = function () {
     description: this.description,
     state: this.state,
     assigneeId: this.assigneeId,
-    label: this.point.toString(),
+    label: (this.point || 0).toString(),
     remainPoint: this.remainPoint,
     ttl: this.ttl,
     startedDate: this.startedDate,
